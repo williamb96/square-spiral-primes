ccd56b6d4235bf3929e97c14cd17df1a68d43c60
William <willeb96@gmail.com>
2015-12-14 21:58:43 +0100

    Fix indentation

--- Primes.java
+++ Primes.java
@@ -57,20 +57,20 @@ public class Primes {
 	}
 	
 	private static boolean isPrime(long num) {
-        if (num == 2) {
-        	return true;
-        }
-        
-        if (num % 2 == 0) {
-        	return false;
-        }
-        
-        for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) {
-            	return false;
-            }
-        }
-        
-        return true;
+		if (num == 2) {
+			return true;
+		}
+		
+		if (num % 2 == 0) {
+			return false;
+		}
+		
+		for (int i = 3; i * i <= num; i += 2) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
